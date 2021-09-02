1b4e9dc21fddce309d1fd740675643c24d64a355
Risyardi Iswanto <elyeel@yahoo.com>
2021-09-02 00:43:43 -0700

    created daylistitem comp&scss, styling the comp using classnames lib

---
+++ src/components/DayListItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'components/DayListItem.scss';
+import classNames from 'classnames';
+
+export default function DayListItem(props) {
+	return (
+		<li onClick={() => props.setDay(props.name)}>
+			<h2 className="text--regular">{props.name}</h2>
+			<h3 className="text--light">{props.spots}</h3>
+		</li>
+	);
+}
