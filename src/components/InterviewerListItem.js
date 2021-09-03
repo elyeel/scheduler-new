e9a949ce20326b0c2efe1927edde95f8acd7abe1
Risyardi Iswanto <elyeel@yahoo.com>
2021-09-03 00:41:34 -0700

    created interviewerlistitem comp, added story for interviewerlistitem

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'components/InterviewerListItem.scss';
+import classNames from 'classnames';
+
+export default function(props) {
+	const interviewerClass = classNames('interviewers__item', {
+		'interviewers__item--selected': props.selected
+	});
+	const imageItemClass = classNames('interviewers__item-image');
+
+	return (
+		<li className={interviewerClass} onClick={() => props.setInterviewer()}>
+			<img className={imageItemClass} src={props.avatar} alt={props.name} />
+			{props.name}
+		</li>
+	);
+}
