4f8f1df5cd289f3fa01f0632c57bd50e215a7cf5
Risyardi Iswanto <elyeel@yahoo.com>
2021-09-09 13:13:35 -0700

    building Appointment, displaying appointments with storybook

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,34 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InterviewerList from 'components/InterviewerList';
 import Button from 'components/Button';
 
 export default function Form(props) {
-	const { name, interviewers, interviewer, onSave, onCancel } = props;
+	const { interviewers, onSave, onCancel } = props;
+	const [ studentName, setStudentName ] = useState(props.name || '');
+	const [ interviewer, setInterviewer ] = useState(props.interviewer || null);
+
+	const reset = () => {
+		setStudentName('');
+		setInterviewer(null);
+	};
+
+	const cancel = () => {
+		reset();
+		onCancel();
+	};
+
+	// const submit = event => event.preventDefault()
 
 	return (
 		<main className="appointment__card appointment__card--create">
 			<section className="appointment__card-left">
-				<form autoComplete="off">
+				<form autoComplete="off" onSubmit={(e) => e.preventDefault()}>
 					<input
 						className="appointment__create-input text--semi-bold"
 						name="name"
 						type="text"
 						placeholder="Enter Student Name"
-						value={name}
+						value={studentName}
+						onChange={(e) => setStudentName(e.target.value)}
 						/*
           This must be a controlled component
         */
 					/>
 				</form>
 				<InterviewerList
-					interviewers={props.interviewers}
-					value={props.interviewer}
-					onChange={props.onChange}
+					interviewers={interviewers}
+					value={interviewer}
+					onChange={setInterviewer}
 				/>
 			</section>
 			<section className="appointment__card-right">
 				<section className="appointment__actions">
-					<Button danger onClick={onCancel}>
+					<Button danger onClick={cancel}>
 						Cancel
 					</Button>
 					<Button confirm onClick={onSave}>
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import Empty from './Empty';
+import Show from './Show';
+import Header from './Header';
 import './styles.scss';
 
-// import Header from './Header';
+// import { Header, Show, Empty } from '../Appointment';
 
-export default function Appointment() {
-	return <article className="appointment" />;
+export default function Appointment({ id, time, interview }) {
+	return (
+		<article className="appointment">
+			<Header time={time} />
+			{!interview && <Empty />}
+			{interview && (
+				<Show student={interview.student} interviewer={interview.interviewer} />
+			)}
+		</article>
+	);
 }
