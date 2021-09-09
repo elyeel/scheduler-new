2f9f35d76ab0e086a5f8fa885a5c6048fd24a1f0
Risyardi Iswanto <elyeel@yahoo.com>
2021-09-08 22:45:49 -0700

    building appointment components, built the status components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './styles.scss';
 
+// import Header from './Header';
+
 export default function Appointment() {
 	return <article className="appointment" />;
 }
--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,12 @@ import DayList from 'components/Daylist';
 import InterviewerListItem from 'components/InterviewerListItem';
 import InterviewerList from 'components/InterviewerList';
 import Appointment from 'components/Appointment';
+import Header from 'components/Appointment/Header';
+import Empty from 'components/Appointment/Empty';
+import Show from 'components/Appointment/Show';
+import Confirm from 'components/Appointment/Confirm';
+import Status from 'components/Appointment/Status';
+import Error from 'components/Appointment/Error';
 
 storiesOf('Button', module)
 	.addParameters({
@@ -141,4 +147,29 @@ storiesOf('Appointment', module)
 		backgrounds: [ { name: 'white', value: '#fff', default: true } ]
 	})
 	.add('Appointment', () => <Appointment />)
-	.add('Appointment with Time', () => <Appointment time="12pm" />);
+	.add('Appointment with Time', () => <Appointment time="12pm" />)
+	.add('Header', () => <Header time="12pm" />)
+	.add('Empty', () => <Empty onAdd={action('onAdd')} />)
+	.add('Show', () => (
+		<Show
+			student={'Lydia Miller-Jones'}
+			interviewer={interviewer}
+			onEdit={action('onEdit')}
+			onDelete={action('onDelete')}
+		/>
+	))
+	.add('Confirm', () => (
+		<Confirm
+			message={'Delete the appointment?'}
+			onConfirm={action('onConfirm')}
+			onCancel={action('onCancel')}
+		/>
+	))
+	.add('Status on delete', () => <Status message={'Deleting...'} />)
+	.add('Status on saving', () => <Status message={'Saving...'} />)
+	.add('Error', () => (
+		<Error
+			message={'Could not delete tha appointment'}
+			onClose={action('onClose')}
+		/>
+	));
