60d0ae65e04db4359af6dd63cba677186bbd47ef
Risyardi Iswanto <elyeel@yahoo.com>
2021-09-13 18:24:03 -0700

    added getInterviewersForDay function and the test for it

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -17,5 +17,5 @@ export function getInterviewersForDay(state, day) {
 	if (state.days.length === 0) return [];
 	const selectedDay = state.days.filter((d) => d.name === day);
 	if (selectedDay.length === 0) return [];
-	return selectedDay[0].appointments.map((dayId) => state.appointments[dayId]);
+	return selectedDay[0].interviewers.map((dayId) => state.interviewers[dayId]);
 }
