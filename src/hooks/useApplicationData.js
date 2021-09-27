1f32b7cc43961617b3315284311d339a4680d5cd
Risyardi Iswanto <elyeel@yahoo.com>
2021-09-26 23:43:24 -0700

    modified updating remaining spots logic

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -26,36 +26,6 @@ let webSocket;
 
 export default function useApplicationData() {
 	function reducer(state, action) {
-		const { id, interview } = action.value;
-
-		const appointment = interview
-			? {
-					...state.appointments[id],
-					interview: { ...interview }
-				}
-			: { ...state.appointments[id], interview };
-		const appointments = {
-			...state.appointments,
-			[id]: appointment
-		};
-
-		//this can be solved by using state.day comparison with state.days to get only the spots in a day to be updated, given state.day is dynamic
-		let dayId = 0;
-		if (id > 5) dayId++;
-		if (id > 10) dayId++;
-		if (id > 15) dayId++;
-		if (id > 20) dayId++;
-
-		const spotsInDay = Object.values(appointments).reduce((a, c, i) => {
-			if (c.interview === null && i >= dayId * 5 && i < dayId * 5 + 5) a++;
-			return a;
-		}, 0);
-
-		//array.map create a new array with new value based on previous
-		const tempDays = state.days.map(
-			(day) => (day.id === dayId + 1 ? { ...day, spots: spotsInDay } : day)
-		);
-
 		switch (action.type) {
 			case SET_DAY:
 				return {
@@ -70,6 +40,29 @@ export default function useApplicationData() {
 					interviewers: action.value.interviewers
 				};
 			case SET_INTERVIEW: {
+				const { id, interview = null } = action.value;
+				// console.log(id, interview);
+
+				const appointment = {
+					...state.appointments[id],
+					interview: interview ? { ...interview } : null
+				};
+				const appointments = {
+					...state.appointments,
+					[id]: appointment
+				};
+
+				//this can be solved by using state.day comparison with state.days to get only the spots in a day to be updated, given state.day is dynamic
+
+				const selectedDay = state.days.find((day) => day.name === state.day);
+				const nullAppts = selectedDay.appointments.reduce((a, c) => {
+					if (appointments[c].interview === null) a++;
+					return a;
+				}, 0);
+				const tempDays = state.days.map(
+					(day) => (day.name === state.day ? { ...day, spots: nullAppts } : day)
+				);
+
 				return {
 					...state,
 					appointments,
